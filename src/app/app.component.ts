df3930f21fcbc35bff237e192f70d9bfb47ebe95
Roman Uriel <roman.francisco@redsinergia.com>
2025-01-15 16:52:17 -0600

    load files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  template: `<h1>Hello word</h1>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
